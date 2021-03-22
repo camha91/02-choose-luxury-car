5ad86652d58946f7ae45c9ed4ed3677413696be0
camha91 <crystalhanguyen@gmail.com>
2021-03-22 00:07:47 -0700

    Apply 360 car rotating

--- src/component/ChooseTeslaSAdvanced/ChooseTeslaSAdvanced.js
+++ src/component/ChooseTeslaSAdvanced/ChooseTeslaSAdvanced.js
@@ -4,12 +4,48 @@ import dataFeatures from "../../Data/arrayFeatures.json";
 import dataWheels from "../../Data/wheels.json";
 
 export default class ChooseTeslaSAdvanced extends Component {
+  // Create state for changing car
+  state = {
+    carCurrent: {
+      id: 1,
+      title: "Pearl White",
+      type: "Multi-Coat",
+      img: "./images/icons/icons/icon-white.jpg",
+      srcImg: "images-white/images-white-1/",
+      color: "White",
+      price: "112,990",
+      engineType: "Tri Motor All-Wheel Drive",
+      range: "390 mi",
+      peakPower: "1,020 hp",
+      acceleration: "1.99 s 0-60 mph",
+      topSpeed: "200 mph",
+      wheels: [
+        {
+          idWheel: 1,
+          srcImg: "images-white/images-white-1/",
+        },
+        {
+          idWheel: 2,
+          srcImg: "images-white/images-white-2/",
+        },
+      ],
+    },
+  };
+
+  changeCar = (newCar) => {
+    return this.setState({
+      carCurrent: newCar,
+    });
+  };
+
   renderIcon = () => {
     return dataFeatures.map((item, index) => {
       return (
         <div
+          onClick={() => this.changeCar(item)}
           className="row border border-color-default mt-2 pt-2 pb-2 m-3"
           key={index}
+          style={{ cursor: "pointer" }}
         >
           <div className="col-3">
             <img
@@ -46,18 +82,57 @@ export default class ChooseTeslaSAdvanced extends Component {
     });
   };
 
+  // Life Cycles
+
+  componentDidMount = () => {
+    // Only run one time after render
+
+    let tagScript = document.createElement("script");
+    tagScript.src =
+      "https://cdn.scaleflex.it/plugins/js-cloudimage-360-view/2.6.0/js-cloudimage-360-view.min.js";
+
+    document.querySelector("#appendScript").appendChild(tagScript);
+  };
+
+  componentDidUpdate = () => {
+    // Run after state changed, automatically run after render
+    // Note: Do not setState at this component because infinity loop
+
+    // Clear old images
+    document.querySelector("#carCurrent").innerHTML = "";
+
+    let tagScript = document.createElement("script");
+
+    tagScript.src =
+      " https://cdn.scaleflex.it/filerobot/js-cloudimage-360-view/v2.0.0.lazysizes.min.js";
+
+    // Clear old script before append new script
+    document.querySelector("#appendScript").innerHTML = "";
+
+    document.querySelector("#appendScript").appendChild(tagScript);
+  };
+
   render() {
     return (
       <div className="container-fluid">
         <div className="row">
           <div className="col-6">
-            <div className="model">
-              <img
+            <div className="model" style={{ width: "100%" }}>
+              {/* <img
                 style={{ width: "100%" }}
                 src="./images/images-white/images-white-1/white-1.png"
                 alt="white_tesla"
-              />
+              /> */}
+              <div
+                id="carCurrent"
+                style={{ minWidth: "100%" }}
+                className="cloudimage-360"
+                data-folder={"./images/" + this.state.carCurrent.srcImg}
+                data-filename="tesla-{index}.jpg"
+                data-amount="8"
+              ></div>
             </div>
+            <div id="appendScript"></div>
             <div className="card mt-2">
               <h5 className="card-header text-default">Exterior Color</h5>
               <table className="table border border-color-light" border="1">
